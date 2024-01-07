2798151263d9dfc43a99d3954e641fbaeb709323
maivankien <maivankien1710@gmail.com>
2024-01-07 11:13:11 +0700

    first commit

---
+++ src/config/config.cpp
@@ -0,0 +1,11 @@
+#include "config.h"
+
+// Config connection
+const char* ssid = "WIFI FREE";
+// const char *password = "khongcho";
+// const char *mqttBroker = "192.168.1.9";
+// const int mqttPort = 1883;
+// const char *mqttUser = "admin";
+// const char *mqttPassword = "123456";
+// const char *topicSubscribe = "mqtt/topic";
+
---
+++ src/config/config.h
@@ -0,0 +1,10 @@
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+extern const char* ssid;  // Tên WiFi
+// extern const char* password;  // Mật khẩu WiFi
+// extern const char* mqttBroker;  // Địa chỉ MQTT broker
+// Thêm các biến cấu hình khác nếu cần
+
+#endif
